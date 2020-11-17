22f9af74c2eafbaa3d89ed898eb0bef64197e48b
mgrddsj <Jesse_Xu@live.com>
2020-11-17 10:36:57 +0800

    Update Projectile.py

--- VPython Works/Projectile.py
+++ VPython Works/Projectile.py
@@ -1,3 +1,4 @@
+import numpy as np
 from vpython import *
 
 ball = sphere(pos=vector(0, 0.1, 0), radius=0.1, color=color.white)
@@ -7,17 +8,24 @@ ball.velocity = vector(0, 5, 0)
 ball.mass = 0.5
 ball.p = ball.velocity*ball.mass
 g = vector(0, -9.8, 0)
-Fnet = g*ball.mass
+c = 2
+rho = 1.2
+A = np.pi*ball.radius**2
+Fnet = (g*ball.mass - 0.5*rho*c*A*mag(ball.p/ball.mass)**2*ball.p/mag(ball.p))
 t_max = 100
 steps = 100000
 dt = t_max/steps
 t = 0
 
+posgraph = gcurve(color=color.green)
+
 while t<t_max:
-    rate(500)
+    rate(1000)
     ball.pos = ball.pos + (ball.p/ball.mass)*dt
     ball.p = ball.p + Fnet*dt
     t = t + dt
 
-    if ball.pos.y < (floor.pos.y + ball.radius):
+    posgraph.plot(pos=(t, ball.pos.y))
+
+    if (ball.pos.y - (floor.pos.y + ball.radius))<0.001:
         ball.p = -ball.p
