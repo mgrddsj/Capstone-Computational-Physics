6bd0b1ab2e67a63dbe806fbd0dddbddbe304f814
mgrddsj <Jesse_Xu@live.com>
2020-12-09 09:11:56 +0800

    Update ThreeBody.py

--- VPython Works/ThreeBody.py
+++ VPython Works/ThreeBody.py
@@ -1,4 +1,4 @@
-from random import Random, randint
+import random as rand
 import numpy as np
 import scipy as sci
 from scipy import integrate
@@ -21,9 +21,12 @@ m1=1.1 #First ball
 m2=0.907 #Second ball
 m3=1.3 #Third ball
 #Define initial position vectors
-r1=[-0.5,0,0] #m
-r2=[0.5,0,0] #m
-r3=[0,1,0] #m
+# r1=[-0.5,0,0] #m
+# r2=[0.5,0,0] #m
+# r3=[0,1,0] #m
+r1=[-rand.random(),0,0] #m
+r2=[rand.random(),0,0] #m
+r3=[0,rand.random(),0] #m
 #Convert pos vectors to arrays
 r1=np.array(r1,dtype="float64")
 r2=np.array(r2,dtype="float64")
@@ -72,7 +75,7 @@ def ThreeBodyEquations(w,t,G,m1,m2,m3):
 #Package initial parameters
 init_params=np.array([r1,r2,r3,v1,v2,v3]) #create array of initial params
 init_params=init_params.flatten() #flatten array to make it 1D
-time_span=np.linspace(0,100,500000) #8 orbital periods and 500 points
+time_span=np.linspace(0,50,50000) #8 orbital periods and 500 points
 #Run the ODE solver
 three_body_sol=sci.integrate.odeint(ThreeBodyEquations,init_params,time_span,args=(G,m1,m2, m3))
 
@@ -82,14 +85,14 @@ r3_sol=three_body_sol[:,6:9]
 
 canvas(background=vector(225/255, 226/255, 225/255), width=1280, height=720, center=vector(0,0,0))
 
-sphere1 = sphere(pos=vector(r1_sol[0,0],r1_sol[0,1],r1_sol[0,2]), radius=0.1, color=color.blue, make_trail=True, shininess=1, texture=textures.metal)
+sphere1 = sphere(pos=vector(r1_sol[0,0],r1_sol[0,1],r1_sol[0,2]), radius=0.1, make_trail=True, texture="minecraft_tree_wood.jpg")
 sphere2 = sphere(pos=vector(r2_sol[0,0],r2_sol[0,1],r2_sol[0,2]), radius=0.1, color=color.red, make_trail=True, shininess=0.5, texture=textures.metal)
 sphere3 = sphere(pos=vector(r3_sol[0,0],r3_sol[0,1],r3_sol[0,2]), radius=0.12, color=color.green, make_trail=True, shininess=0.1, texture=textures.metal)
 
 
 i = 1
 while (i<len(r1_sol)):
-    rate(5000)
+    rate(1000)
     sphere1.pos = vector(r1_sol[i,0],r1_sol[i,1],r1_sol[i,2])
     sphere2.pos = vector(r2_sol[i,0],r2_sol[i,1],r2_sol[i,2])
     sphere3.pos = vector(r3_sol[i,0],r3_sol[i,1],r3_sol[i,2])
