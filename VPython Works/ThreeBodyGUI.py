3dcbb499ad42e4c6927afa8da8f8dfd45bd9760d
mgrddsj <Jesse_Xu@live.com>
2021-03-17 08:42:18 +0800

    Revert "Added Multiprocessing, does not work with vpython."
    This reverts commit 66dfbcad319900185d4415dbc6a91942542d67f0.

--- VPython Works/ThreeBodyGUI.py
+++ VPython Works/ThreeBodyGUI.py
@@ -5,7 +5,7 @@ from scipy import integrate
 from vpython import *
 from ThreeBodyMotionSolver import *
 from timeit import default_timer as timer
-from multiprocessing import Process, Manager
+import threading
 
 # Prepare variables
 # Define masses
@@ -46,14 +46,7 @@ def generate(b):
     trail3.clear()
 
     start = timer()
-    result = []
-    with Manager() as manager:
-        p = Process(target=solveThreeBodyMotion, args=(m1, m2, m3, r1, r2, r3, result))
-        p.start()
-        print("process started")
-        p.join()
-        print("process finished")
-    r1_sol, r2_sol, r3_sol = result[0], result[1], result[2]
+    r1_sol, r2_sol, r3_sol = solveThreeBodyMotion(m1, m2, m3, r1, r2, r3)
     end = timer()
     print("finished calculating, time elapsed:", (end-start))
     sphere1.pos = vector(r1_sol[0,0],r1_sol[0,1],r1_sol[0,2])
@@ -122,9 +115,13 @@ r3zinput = winput(prompt="r3.z:", text=0, pos=scene.title_anchor, bind=setr3z)
 
 print("Started calculating")
 start = timer()
-result = []
-solveThreeBodyMotion(m1, m2, m3, r1, r2, r3, result)
-r1_sol, r2_sol, r3_sol = result[0], result[1], result[2]
+r1_sol, r2_sol, r3_sol = solveThreeBodyMotion(m1, m2, m3, r1, r2, r3)
+end = timer()
+print("finished calculating, time elapsed:", (end-start))
+
+print("Started second round of calculating")
+start = timer()
+solveThreeBodyMotion(m1, m2, m3, r1, r2, r3)
 end = timer()
 print("finished calculating, time elapsed:", (end-start))
 
